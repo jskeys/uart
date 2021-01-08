966f0e0d1bd001ca25b8ae60423cace833a7cb40
jon <jssass@gmail.com>
2021-01-08 12:36:44 +0000

    Improve FTDI D2XX download script
    The FTDI example should compile on different linux targets.  Thererfore,
    this commit adds a step to query the processor architecture and
    downloads the correct library.

--- download.sh
+++ download.sh
@@ -1,2 +1,14 @@
-wget https://www.ftdichip.com/Drivers/D2XX/Linux/libftd2xx-arm-v7-hf-1.4.8.gz
-tar -xaf ./libftd2xx-arm-v7-hf-1.4.8.gz
+ARCH=`uname -m`
+
+URL="https://www.ftdichip.com/Drivers/D2XX/Linux/"
+
+case $ARCH in
+    armv7l)
+        FILE="libftd2xx-arm-v7-hf-1.4.8.gz"
+        ;;
+    x86_64)
+        FILE="libftd2xx-x86_64-1.4.8.gz"
+esac
+
+wget ${URL}${FILE}
+tar -xaf ./${FILE}
